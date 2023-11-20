adb2ef1c376bba36afe83be4fc23028d97d19341
Jessie Stewart <jmstewart0717@gmail.com>
2023-11-20 13:39:18 -0500

    First commit

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,19 @@
+"""This module analyzes the sentiment of a given input"""
+
+import json
+import requests
+
+def sentiment_analyzer(text_to_analyse):
+    """This function analyzes the sentiment"""
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    return {'label': label, 'score': score}
